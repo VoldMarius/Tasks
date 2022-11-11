8de24bb40dd42708de2f5beb66c7173cf16ba12c
VoldMarius <ukusi@mail.ru>
2022-11-11 15:27:00 +0300

    My solution

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 00 :
+//Написать программу, которая принимает число и выдает его кватдрат 
+
+//Например:
+// 4 -> 16
+//-3 -> 9
+//-7 -> 49
+
+
+Console.WriteLine("Введите целое число:  ");
+int num = Convert.ToInt32 (Console.ReadLine());
+int sqv = num * num;
+Console.WriteLine(sqv);
+
+ 
