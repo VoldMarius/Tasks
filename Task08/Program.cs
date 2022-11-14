d5144e0b2df6949b69aae2855fb68556da84c927
VoldMarius <ukusi@mail.ru>
2022-11-14 15:40:39 +0300

    new vew Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,13 +2,19 @@
 // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // // 5 -> 2, 4
 // // 8 -> 2, 4, 6, 8
-Console.Write("Введите число N:  ");
-int n = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-while (count <= n)
-{
-    if (count % 2 == 0)
+// Console.Write("Введите число N:  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (count <= n)
+// {
+//     if (count % 2 == 0)
 
-        Console.Write($"{count}, ");
-    count++;
+//         Console.Write($"{count}, ");
+//     count++;
+// }
+Console.Write("Введите число N:  ");
+int i = 2;
+for (int n = Convert.ToInt32(Console.ReadLine()); i < n; i += 2)
+{
+    Console.Write(i + " ");
 }
