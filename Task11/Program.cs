0bf408cf250c41c2785e6189cec018bc9f0cebae
VoldMarius <ukusi@mail.ru>
2022-11-15 14:36:47 +0300

    Task11 Task10

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+
+int DeliteSecondDig(int num)
+{
+    int b = num % 10;
+    int a = num / 100 * 10 + b;
+    return a;
+}
+int delSecDig = DeliteSecondDig(number);
+Console.WriteLine($"число = {number} --> {delSecDig}");
