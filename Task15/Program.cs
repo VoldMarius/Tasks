ad717b376fbfec7675240ee618946dfb5393daed
VoldMarius <ukusi@mail.ru>
2022-11-15 22:03:03 +0300

    Ефыл15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,11 +4,11 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-Console.Write("Введите число о 1 до 7:  ");
+Console.Write("Введите число от 1 до 7:  ");
 int digit = Convert.ToInt32(Console.ReadLine());
-bool Wikend (int n)
+bool Weekend (int n)
 {
 return (n>0 && n<6);
 }
 
-Console.Write(Wikend(digit) ? "Нет" : "Да");
+Console.Write(Weekend(digit) ? "Нет" : "Да");
