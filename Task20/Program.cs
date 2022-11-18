5fa45de584ebccd99a21239c9e4fc958cc211f76
VoldMarius <ukusi@mail.ru>
2022-11-18 11:31:15 +0300

    Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,32 @@
+﻿// int n = 12345;
+// double res = Math.Sqrt(n);
+
+// double n1 = 5.099985645;
+// double nRound = Math.Round(n1, 2, MidpointRounding.ToZero);
+// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите координаты 2 точки ");
+Console.Write("X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты 1 точки ");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+double Distance (int xc,int yc, int xc1,int yc1)
+{
+double dist = Math.Sqrt((xc1-xc)*(xc1-xc)+(yc1-yc)*(yc1-yc));
+double res = Math.Round(dist, 2, MidpointRounding.ToZero);
+return res;
+}
+
+Console.WriteLine (Distance (x, y, x1,y1));
