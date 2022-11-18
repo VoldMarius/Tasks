c2fd3dee35e982c89a87ecce9bca2137a8945dd5
VoldMarius <ukusi@mail.ru>
2022-11-18 22:23:11 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = num * -1;
+}
+Square(num);
+
+void Square(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i} -> {i * i * i} ");
+    }
+}
