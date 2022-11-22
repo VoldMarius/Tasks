98a0420ab76baf14538c6fb88918f10a431d565d
VoldMarius <ukusi@mail.ru>
2022-11-22 14:25:58 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+Console.WriteLine("Введите число A");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SNumber(int n)
+{
+    int rezult = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        rezult = rezult * i;
+
+    }
+    return rezult;
+
+}
+int sNumber= SNumber(number);
+if (number > 0)
+    Console.WriteLine(sNumber);
+else   
+    Console.WriteLine("Введите число A");
---
+++ Task30/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// int[] array = new int[8];
+// int[] array1 = { 3, 5, 7, 8 };
+
+
+// Random rnd = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rnd.Next();
+// }
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+
+// Console.WriteLine(string.Join(" ", array));
+PrintArray(array);
