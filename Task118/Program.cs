797f6292583de3610d08c3b0aef1243e41c06739
VoldMarius <ukusi@mail.ru>
2022-11-25 10:06:54 +0300

    Task118

--- Task118/Program.cs
+++ Task118/Program.cs
@@ -1,2 +1,49 @@
 ﻿//  В заданном массиве действительных чисел найдите 
 //  разность между максимальным и минимальным числом.
+Console.Write("Введите число X- кол-во элементов массива  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[x];
+void RandomArray(int[] array)//Метод обьявления массива случайными элементами
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+}
+RandomArray(arr);
+
+void PrintArray(int[] array)//Метод вывода массива
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]},");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+Console.WriteLine();
+void MaxArray(int[] arrayM)// Метод сортировки массива
+{
+    int max = arrayM[0];
+    for (int i = 1; i < arrayM.Length; i++)
+    {
+
+        if (arrayM[i] > arrayM[i-1])
+        {
+            max = arrayM[i];
+        }
+    }
+Console.Write($"Максимальное элемент массива:{max}");
+}
+MaxArray(arr);
+
+
+
