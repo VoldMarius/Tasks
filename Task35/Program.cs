90081c827f2219d6d84251f4eea035761d776215
VoldMarius <ukusi@mail.ru>
2022-11-25 14:46:43 +0300

    Task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -6,3 +6,37 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+int[] CreateRandomArray(int size, int min, int max)//Метод обьявления массива случайными элементами
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array, string elem1, string elem2)//Метод вывода массива
+{
+    Console.Write(elem1);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]},");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write(elem2);
+}
+int[] arr = CreateRandomArray(10, 9, 100);
+int FindElement(int[] array, int min, int max)
+{
+    int findElementcount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > min && array[i] < max) findElementcount++;
+    }
+    return findElementcount;
+}
+PrintArray(arr, "[", "] -->");
+Console.Write(FindElement(arr, 9, 100));
