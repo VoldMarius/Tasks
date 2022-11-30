e59b90f730af5206d8e7b38ccdf307e8bd483cc2
VoldMarius <ukusi@mail.ru>
2022-11-30 11:07:25 +0300

    Task41-43

---
+++ Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите планируемое для ввода кол-во чисел М  ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[m];
+int number = 1;
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Введите {number} число  ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    number++;
+}
+int NumberPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] array, string elem1, string elem2)
+{
+    Console.Write(elem1);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]},");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write(elem2);
+}
+PrintArray(arr, "[", "] -->");
+Console.Write(NumberPositive(arr));
---
+++ Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//   значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] FindXY(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * (b2 - b1 )/ (k1 - k2 )+ b1;
+    double[] array = new double[2];
+    array[0]=x;
+    array[1]=y;
+    return array;
+}
+void PrintArray(double[] array, string elem1, string elem2)
+{
+    Console.Write(elem1);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{Math.Round((array[i]), 1, MidpointRounding.ToZero)}; ");
+        else
+            Console.Write($"{Math.Round((array[i]), 1, MidpointRounding.ToZero)}");
+    }
+    Console.Write(elem2);
+}
+Console.WriteLine("Введите значение переменной b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение переменной k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение переменной k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double[]arr= FindXY(b1, b2, k1, k2);
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -->  ");
+ PrintArray(arr, "(", ")");
