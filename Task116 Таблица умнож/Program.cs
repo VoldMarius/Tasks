29bab02bff292e4ef4f887f33e6c73d34407c18a
VoldMarius <ukusi@mail.ru>
2022-11-30 16:09:34 +0300

    Task41

--- Task116 Таблица умнож/Program.cs
+++ Task116 Таблица умнож/Program.cs
@@ -1,18 +1,39 @@
-﻿// Напишите программу, которая выводит на консоль таблицу умножения.
-// Реализовать через массив и метод 
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
 
-int[] array = new int[10] {1,2,3,4,5,6,7,8,9,10};
-void Table(int[]arr)
+Console.WriteLine("Введите сколько пожелаете целых чисел  через пробел");
+string str = Console.ReadLine();
+int[] ConvertStrToInt(string arg)
 {
-for (int i = 0; i <arr.Length; i++)
+int[] a = arg.Split(' ').Select(int.Parse).ToArray();
+return a;
+}
+void PrintArray(int[] array, string elem1, string elem2)
 {
-    for (int j = 1; j <arr.Length; j++)
+    Console.Write(elem1);
+    for (int i = 0; i < array.Length; i++)
     {
-        int rezult = i*j;
-        Console.Write("{0}*{1}={2}\t",i,j,rezult);
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
     }
-    Console.WriteLine();
+    Console.Write(elem2);
 }
-
+int NumberPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
 }
-Table(array);
+PrintArray(ConvertStrToInt(str), "[", "] -->  ");
+int result = NumberPositive(ConvertStrToInt(str));
+Console.Write(result);
--- Task41/Program.cs
+++ Task41/Program.cs
@@ -2,9 +2,10 @@
 // сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+//Основное решение
 Console.WriteLine("Введите планируемое кол-во чисел М  ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[]InputNumbers(int plannedNumber)
+int[]InputNumbers(int plannedNumber)//Метод ввода М чисел 
 {
 int[] array = new int[plannedNumber];
 int number = 1;
@@ -15,7 +16,7 @@ for (int i = 0; i < plannedNumber; i++)
     number++;
 }return array;
 }
-int NumberPositive(int[] array)
+int NumberPositive(int[] array)//Метод поиска положительных чисел массива
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
@@ -42,3 +43,40 @@ void PrintArray(int[] array, string elem1, string elem2)
 int[] arr= InputNumbers(m);
 PrintArray(arr, "[", "] -->");
 Console.Write(NumberPositive(arr));
+
+
+//Решение с испольхзованием стороних методов
+// Console.WriteLine("Введите сколько пожелаете целых чисел  через пробел");
+// string str = Console.ReadLine();
+// int[] ConvertStrToInt(string arg)//Экспереметальный метод преобразования строки в массив
+// {
+// int[] a = arg.Split(' ').Select(int.Parse).ToArray();
+// return a;
+// }
+// void PrintArray(int[] array, string elem1, string elem2)
+// {
+//     Console.Write(elem1);
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1)
+//             Console.Write($"{array[i]}, ");
+//         else
+//             Console.Write($"{array[i]}");
+//     }
+//     Console.Write(elem2);
+// }
+// int NumberPositive(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// PrintArray(ConvertStrToInt(str), "[", "] -->  ");
+// int result = NumberPositive(ConvertStrToInt(str));
+// Console.Write(result);
